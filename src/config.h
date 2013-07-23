eb15f5e8b02cf6c88373e2e9ecefc508d8942782
Marc Juul <juul@labitat.dk>
2013-07-23 01:53:48 -0700

    switched the code to use libhttpd

---
+++ src/config.h
@@ -0,0 +1,14 @@
+/* Define if you have the <stdarg.h> header file.  */
+#define HAVE_STDARG_H 1
+
+/* Define if you have the <string.h> header file.  */
+#define HAVE_STRING_H 1
+
+/* Define if you have the <strings.h> header file.  */
+#define HAVE_STRINGS_H 1
+
+/* Define if you have the <unistd.h> header file.  */
+#define HAVE_UNISTD_H 1
+
+/* Define if you have the nsl library (-lnsl).  */
+#undef HAVE_LIBNSL
--- src/main.c
+++ src/main.c
@@ -10,23 +10,25 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <string.h>
+#include <httpd.h>
 
 #define APPNAME "internetisdownredirect"
 #define APPVERSION "0.1"
-#define LISTEN_PORT 3000
-#define CONNECTION_MAX_BACKLOG 64
-#define MAXRECV 4096
-#define REDIRECTMSG "HTTP/1.1 302 See Other\nLocation: http://sudoroom.org/\n\n"
-#define HTTP_REDIRECT_MSG "HTTP/1.1 302 See Other\nLocation: "
 
-char* response;
+#define PROXY_HOST "localhost\0"
+
+int debug_mode;
+httpd* server;
+char* redirect_url;
+char* location_header;
 
 void version() {
   fprintf(stderr, "%s version: %s", APPNAME, APPVERSION);
 }
 
 void usage(char* cmd_name) {
-  fprintf(stderr, "%s: <port> <redirect_url> [debug]\n", cmd_name);
+  fprintf(stderr, "%s: <ip> <port> <redirect_url> [debug]\n", cmd_name);
+  fprintf(stderr, "    <ip> is the IP where %s binds and listens. Put ALL to bind to all IPs. \n", cmd_name);
   fprintf(stderr, "    <port> is the port where the %s listens (required).\n", cmd_name);
   fprintf(stderr, "    <redirect_url> is the url to which http GET and POST\n");
   fprintf(stderr, "     requests are redirected (required).\n");
@@ -40,196 +42,174 @@ void fail(char* error) {
 }
 
 void debug(char* dbg) {
-
+  if(debug_mode > 0) {
+    printf("[debug]: %s", dbg);
+  }
 }
 
-int read_and_respond(int fd) {
-  char buffer[MAXRECV];
-  int nbytes;
+// do we have a host-specific redirect?
+char* host_specific_redirect(char* hostname) {
+  // TODO actually implement this
+  return NULL;
+}
 
-  memset(buffer, 0, MAXRECV);
-  
-  nbytes = read(fd, buffer, MAXRECV);
-  debug("Reading\n");
+void redirect() {
 
-  if(nbytes < 0) { // ERROR
-    close(fd);
-    perror("read error");
-    return -1;
+  char* hostname = NULL;
+  char* end;
+  int hostname_length = 0;
+  char* location = NULL;
 
-  } else if(nbytes == 0) { // EOF
-    debug("Connection closed\n");
-    return -1;
-
-  } 
-
-  if((strncmp("GET", buffer, 3) == 0) || 
-     (strncmp("POST", buffer, 4) == 0)) {
-    
-    if(send(fd, REDIRECTMSG, sizeof(REDIRECTMSG), 0) < 0) {
-      perror("send error");
-      return -1;
-    }
-
-  } else {
-    debug("unknown query\n");
-    return -1;
+  // only respond to http get
+  if(server->request.method != HTTP_GET) {
+		httpdEndRequest(server);
   }
 
+  // code for host-specific redirect
+  // the idea is to redirect to mesh-hosted
+  // alternatives to popular services
+  // when the mesh is down
+  // TODO not yet fully implemented
+  if(server->request.host) {
+    end = index(server->request.host, ':');
+    if(!end) {
+      end = index(server->request.host, '\r');
+    }
+    if(!end) {
+      end = index(server->request.host, '\n');
+    }
+    if(!end) {
+      end = index(server->request.host, '\0');
+    }
+    if(!end) {
+      fail("could not parse hostname");
+    }
 
-  return 0;
+    hostname_length = end - (server->request.host);
+    hostname = malloc(hostname_length + 1);
+    strncpy(hostname, server->request.host, hostname_length);
+    hostname[hostname_length] = '\0';
+    
+    location = host_specific_redirect(hostname);
+  }
+
+  if(debug_mode) {
+    if(hostname) {
+      printf("Host: %s\n", hostname);
+    } else {
+      printf("Request did not contain a hostname.\n");
+    }
+    if(location) {
+      printf("Host-specific redirect: %s\n", location);
+    }
+  }
+
+  if(location) {
+    printf("UNIMPLEMENTED: Host-specific redirect\n");
+  }
+
+  httpdSetResponse(server, "302 Found");
+  httpdAddHeader(server, location_header);
+  httpdAddHeader(server, "Connection: close");
+  httpdSendHeaders(server);
 }
 
-int run(port) {
+int run(int port, char* ip, char* redirect_url) {
 
-  int listener;
-  int keep_running = 1;
   int result;
-  fd_set fds;
-  fd_set cur_fds;
+	struct timeval timeout;
 
-  listener = socket(AF_INET, SOCK_STREAM, 0);
-  if(listener < 0) {
-    fail("unable to open socket");
-  }
-  
-  int one = 1;
-  if(setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, (char*) &one, sizeof(one)) < 0) {
-    close(listener);
-    fail("failed to set SO_RESUEADDR on socket");
+
+  if(strcmp(ip, "ALL") == 0) {
+    server = httpdCreate(NULL, port);
+  } else {
+    server = httpdCreate(ip, port);
   }
 
-  // Set non-blockingg
-  int flags = fcntl(listener, F_GETFL, 0);
-  if(flags == -1) {
-    close(listener);
-    fail("Failed to get socket flags");
+	if(server == NULL) {
+		fail("Can't create server");
+	}
+
+  if(debug_mode) {
+    httpdSetAccessLog(server, stdout);
   }
 
-  if(fcntl(listener, F_SETFL, flags |= O_NONBLOCK) < 0) {
-    close(listener);
-    fail("Failed to set non-blocking mode");
+	httpdSetErrorLog(server, stderr);
+
+  httpdAddCWildcardContent(server, "/", NULL, redirect);
+
+  while(1) {
+		timeout.tv_sec = 5;
+		timeout.tv_usec = 0;
+		result = httpdGetConnection(server, &timeout);
+
+		if(result == 0) {
+			continue;
+		}
+
+		if(result < 0) {
+      fprintf(stderr, "error opening incoming connection\n");
+			continue;
+		}
+
+		if(httpdReadRequest(server) < 0) {
+			httpdEndRequest(server);
+			continue;
+		}
+
+		httpdProcessRequest(server);
+		httpdEndRequest(server);
   }
-
-  struct sockaddr_in listen_addr;
-  memset(&listen_addr, 0, sizeof(listen_addr));
-  listen_addr.sin_family      = AF_INET;
-  listen_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  listen_addr.sin_port        = htons(LISTEN_PORT);
-
-  if(bind(listener, (struct sockaddr*) &listen_addr, sizeof(listen_addr)) < 0) {
-    close(listener);
-    fail("bind failed");
-   }
-
-  if(listen(listener, CONNECTION_MAX_BACKLOG) < 0) {
-    close(listener);
-    fail("listen failed");
-  }
-
-  FD_ZERO(&fds);
-  FD_SET(listener, &fds);
-
-  int max_fd = listener;
-
-  struct timeval timeout;
-  timeout.tv_sec = 10;
-  timeout.tv_usec = 0;
-  
-
-  struct sockaddr_in client_addr;
-  size_t size;
-
-  while(keep_running) {
-
-    cur_fds = fds;
-    result = select(FD_SETSIZE, &cur_fds, NULL, NULL, &timeout);
-
-    // reset timer
-    timeout.tv_sec = 10;
-    timeout.tv_usec = 0;
-    
-    if(result < 0) {
-      close(listener);
-      fail("select failed");
-    } else if(result == 0) {
-      // request timed out
-    }
-
-    int i;
-    for(i=0; (i <= max_fd) && (result > 0); i++) {
-      if(!FD_ISSET(i, &cur_fds)) {
-        continue;
-      }
-      if(i == listener) { // connection request
-
-        int con;
-        size = sizeof(client_addr);
-        con = accept(listener, (struct sockaddr *) &client_addr, &size);
-
-        if(con < 0) {
-          perror("accepting incoming connection failed");
-          continue;
-        }
-        debug("Incoming connection accepted\n");
-
-        //        fprintf(stderr,
-        //                "Server: connect from host %s, port %hd.\n",
-        //                inet_ntoa(client_addr.sin_addr),
-        //                ntohs(client_addr.sin_port));
-
-        FD_SET(con, &fds);
-        if(con > max_fd) {
-          max_fd = con;
-        }
-
-      } else { // data on connected socket
-        read_and_respond(i);
-        close(i);
-        FD_CLR(i, &fds);
-      }
-    }
-    
-
-  }
-
-  close(listener);
-
-	return 0;
 }
-
-
 
 int main(int argc, char* argv[]) {
+
+  char* ip;
+
+  debug_mode = 0;
   
-  if(argc >= 2) {
+  if(argc == 2) {
     if(strncmp("-h", argv[1], 2) == 0) {
       usage(argv[0]);
       return 0;
     } else if(strncmp("-v", argv[1], 2) == 0) {
       version(argv[0]);
       return 0;
+    } else {
+      usage(argv[0]);
+      return -1;
     }
   }
 
-  if(argc != 3) {
+  if((argc <= 1) || (argc > 5)) {
     usage(argv[0]);
     return -1;
   }
 
-  int port = atoi(argv[1]);
+  int port = atoi(argv[2]);
   if((port < 1) || (port > 65535)) {
     fprintf(stderr, "Port out of range\n");
     usage(argv[0]);
     return -1;
   }
 
-  response = malloc(strlen(HTTP_REDIRECT_MSG)+strlen(argv[2])+2);
-  strncpy(response, HTTP_REDIRECT_MSG, strlen(HTTP_REDIRECT_MSG));
-  strncpy(response+strlen(HTTP_REDIRECT_MSG), argv[2], strlen(argv[2]));
-  snprintf(response+strlen(HTTP_REDIRECT_MSG)+strlen(argv[2]), 3, "\n\n\0");
+  if(argc == 5) {
+    if(strncmp(argv[4], "debug", 5) != 0) {
+      usage(argv[0]);
+      return -1;
+    }
+    debug_mode = 1;
+  }
 
-  int ret = run(port);
+  ip = argv[1];
+  redirect_url = argv[3];
+
+  location_header = malloc(strlen("Location: \0")+strlen(redirect_url)+1);
+  sprintf(location_header, "Location: %s", redirect_url);
+
+  int ret = run(port, ip, redirect_url);
+
+  free(location_header);
 
   return ret;
   
